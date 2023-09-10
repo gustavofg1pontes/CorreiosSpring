d05b6faed6761eb6809296d2e3fe557abb8c724f
gustavofg1pontes <gustavo.andre@aluno.ifsp.edu.br>
2023-09-10 12:58:38 -0300

    change controller's path

--- src/main/java/com/gustavo/Correios2/controllers/PessoaController.java
+++ src/main/java/com/gustavo/Correios2/controllers/PessoaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
